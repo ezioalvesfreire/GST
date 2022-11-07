85125d76a963b7b839ae7a832d7b132809e8b45d
ezioalvesfreire <ezio.freire@catolicasc.org.br>
2022-11-07 19:36:02 -0300

    acicionado botão flutuante

--- src/pages/DataFets.js
+++ src/pages/DataFets.js
@@ -1,14 +1,20 @@
 import React from "react";
 import './DataFets.css';
 import FormMosfet from "../components/FormMosfet";
-import { Container, Form, Input } from "reactstrap";
+import { Button, Container, Form, Input } from "reactstrap";
 import NavComponent from "../components/NavComponent";
+import { Check, RefreshCcw } from "react-feather";
 
 
 
 const DataFets = () => {
 
     return (
+
+        <>
+            <Button color="primary" style={{position: 'fixed', right: '50px', bottom: '50px', borderRadius: '50px', height: '100px', width: '100px'}} >
+                <RefreshCcw size={30} />
+            </Button>
 
         <Container className="dataTransistor ">
             <NavComponent />
@@ -27,6 +33,7 @@ const DataFets = () => {
                 </div>
             </div>
         </Container>
+        </>
     );
 }
 
