7c3741df8b88b7c4ccc958fa461ce0a10ebfd8b0
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2022-11-16 19:23:08 -0300

    configurando formulário para enviar dados para API

--- src/components/FormMosfet.js
+++ src/components/FormMosfet.js
@@ -9,19 +9,19 @@ let transistors =
 {
     mosfets: {
         mosfet1: {
-            id_mosfet:1,
+            id_mosfet: 1,
             description: "FQP12P20",
-            package: "TO-220",
-            channel: "N",            
+            pack: "TO-220",
+            ch: "N",
             drainCurreunt: "12,5",
             voltageDrainSource: "200",
             resistanceDraiSource: "0,15"
         },
         mosfet2: {
-            id_mosfet:2,
+            id_mosfet: 2,
             description: "FQP19P20",
-            package: "TO-220",
-            channel: "P",            
+            pack: "TO-220",
+            ch: "P",
             drainCurreunt: "11,5",
             voltageDrainSource: "200",
             resistanceDraiSource: "0,47"
@@ -30,24 +30,24 @@ let transistors =
     },
     powerTransistors: {
         powerTransistor1: {
-            id_powerTransistor:3,
+            id_powerTransistor: 3,
             description: "TIP41C",
-            package: "TO-220",
-            channel: "NPN",
+            pack: "TO-220",
+            ch: "NPN",
             voltageCollectorEmitter: 100,
-            voltageEmmiterBase: 5, 
-            collectorCurrent:6 
-                      
+            voltageEmmiterBase: 5,
+            collectorCurrent: 6
+
         },
         powerTransistor2: {
             id_powerTransistor: 4,
             description: "TIP42C",
-            package: "TO-220",
-            channel: "PNP",
+            pack: "TO-220",
+            ch: "PNP",
             voltageCollectorEmitter: 100,
-            voltageEmmiterBase: 5, 
-            collectorCurrent: 6  
-                      
+            voltageEmmiterBase: 5,
+            collectorCurrent: 6
+
         }
     }
 };
@@ -58,60 +58,45 @@ const FormMosfet = () => {
 
     return (
         <div className="TR">
-            <Form>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Description</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder={t} type="text" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Package</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="TO-220" type="text" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Channel</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="lg" type="select">
-                            <option>N-CHANNEL</option>
-                            <option>P-CHANNEL</option>
-                        </Input>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Vds</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="200" type="number" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Id</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="12,3" type="number" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>P tot</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="140" type="number" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Tj</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="150" type="number" />
-                    </Col>
-                </FormGroup>
-
-                <FormGroup row>
-                    <Label for="exampleEmail" size="lg" sm={smLabel}>Rds/on</Label>
-                    <Col sm={smCol}>
-                        <Input bsSize="lg" id="exampleEmail" name="email" placeholder="0,15" type="number" />
-                    </Col>
-                </FormGroup>
-            </Form>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Description</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="descript" name="descript" placeholder={t} type="text" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Package</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="pack" name="pack" placeholder="TO-220" type="text" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Channel</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="ch" name="ch" type="select">
+                        <option>N-CHANNEL</option>
+                        <option>P-CHANNEL</option>
+                    </Input>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Id</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="drainCurreunt" name="drainCurreunt" placeholder="200" type="number" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Vds</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="voltageDrainSource" name="voltageDrainSource" placeholder="12,3" type="number" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="exampleEmail" size="lg" sm={smLabel}>Rds/on</Label>
+                <Col sm={smCol}>
+                    <Input bsSize="lg" id="resistanceDraiSource" name="resistanceDraiSource" placeholder="140" type="number" />
+                </Col>
+            </FormGroup>
         </div>
     );
 }
--- src/pages/DataFets.js
+++ src/pages/DataFets.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DataFets.css';
+import axios from "axios";
 import FormMosfet from "../components/FormMosfet";
 import { Button, Container, Form, Input } from "reactstrap";
 import NavComponent from "../components/NavComponent";
@@ -9,30 +10,67 @@ import { Check, RefreshCcw } from "react-feather";
 
 const DataFets = () => {
 
+    const handleSubmit = async (e) => {//COLOQUEI O async
+        e.preventDefault();
+
+        //PEGO TODOS OS DADOS DO Form1
+        const formData = new FormData(document.getElementById("Form1", e.target))//DADOS DO FORM 1 QUE ESTA DENTRO DO e.target QUE É O FORM SUBMIT
+        const data = Object.fromEntries(formData)
+        console.log("TODOS OS DADOS DO FORM 1", data)
+        //alert(data1);
+
+        //PEGO TODOS OS DADOS DO Form2
+        const formData2 = new FormData(document.getElementById("Form2", e.target))//DADOS DO FORM 2 QUE ESTA DENTRO DO e.target QUE É O FORM SUBMIT
+        const data2 = Object.fromEntries(formData2)
+        console.log("TODOS OS DADOS DO FORM 2", data2);
+        //alert(data2);
+
+        const addPost = data = axios.post("http://localhost:5000/mosfets", data)
+            .then(() => {
+
+                console.log("Sucesso", addPost)
+            }).catch(() => {
+                console.log("Erro", addPost)
+            })
+
+        const addPost2 = data2 = axios.post("http://localhost:5000/mosfets", data2)
+            .then(() => {
+
+                console.log("Sucesso", addPost2)
+            }).catch(() => {
+                console.log("Erro", addPost2)
+            })
+
+    }
+
     return (
 
         <>
-            <Button color="primary" style={{position: 'fixed', right: '50px', bottom: '50px', borderRadius: '50px', height: '100px', width: '100px'}} >
+            <Button color="primary" style={{ position: 'fixed', right: '50px', bottom: '50px', borderRadius: '50px', height: '100px', width: '100px' }} >
                 <RefreshCcw size={30} />
             </Button>
 
-        <Container className="dataTransistor ">
-            <NavComponent />
-            <div className="frmComparator box-sidebar">
-                <div className="TR-01 py-2  px-3">
-                    <div class="col-12 ">
-                        <h2>MOSFET-01</h2>
-                        <FormMosfet />
+            <Container className="dataTransistor">
+                <NavComponent />
+                <Form onSubmit={handleSubmit}>
+                    <div className="frmComparator box-sidebar">
+                        <div className="TR-01 py-2  px-3">
+                            <Form id='Form1' class="col-12 ">
+                                <h2>MOSFET-01</h2>
+                                <FormMosfet/>
+                            </Form>
+                        </div>
+                        <div className="TR-02 py-2  px-3">
+                            <Form id='Form2' class="col-12">
+                                <h2>MOSFET-02</h2>
+                                <FormMosfet/>
+                            </Form>
+
+                        </div>
+                        <Button type="submit">Buscar/ Comparar</Button>
                     </div>
-                </div>
-                <div className="TR-02 py-2  px-3">
-                    <div class="col-12">
-                        <h2>MOSFET-02</h2>
-                        <FormMosfet />
-                    </div>
-                </div>
-            </div>
-        </Container>
+                </Form>
+            </Container>
         </>
     );
 }
